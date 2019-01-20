9122d0be89821a387e86ae0a2f8cde18c5f111e4
[JAM]Andreas <andreas.edmeier@web.de>
2019-01-20 13:58:59 +0100

    Localize_text Text -> Text[] und Dokumentation

--- UnityTools/Assets/Code/Tools/Localization/Localize_text.cs
+++ UnityTools/Assets/Code/Tools/Localization/Localize_text.cs
@@ -5,35 +5,44 @@ using UnityEngine.UI;
 
 
 /// <summary>
-/// Localisation scripts must always have a public get list of Scriptreferences named 
+/// Localisation scripts must always have a public get list of Scriptreferences named references
+/// Localisation scripts must always have a public void funktion caled ChangeLanguage(string Language)
+/// this script requiers the CSVReader
 /// </summary>
 public class Localize_text : MonoBehaviour {
 
     #region ===== ===== DATA ===== =====
 
+    /// <summary>
+    /// a list of all referenced instances of this script
+    /// </summary>
     public static List<Localize_text> references { get; private set; } = new List<Localize_text>();
 
-    [SerializeField] Text _textFeald;
-    [SerializeField] string _streamingAssetPath;
-    [SerializeField] string _id;
-
-    bool _errorFlag = false;
+    [SerializeField] Text[] _textFeald; //reference to the Textfiel
+    [SerializeField] string _streamingAssetPath; //the path to the csv file whitch is used for the Localisation
+    [SerializeField] string _id; //the id of the string
 
     #endregion
     #region ===== ===== REFRENCING ===== =====
 
+    /// <summary>
+    /// registers this script to the static List if it is valide
+    /// if _textFeald is not set, it tryes to find a text component in the gameobject
+    /// </summary>
     private void Awake() {
-        if (!_textFeald) {
-            _textFeald = GetComponent<Text>();
+        if (_textFeald.Length == 0) {
+            _textFeald = new Text[1] { GetComponent<Text>() };
 
-            if (!_textFeald) {
-                _errorFlag = true;
+            if (_textFeald.Length == 0) {
                 return;
             }
         }
         references.Add(this);
     }
 
+    /// <summary>
+    /// unregisters this script
+    /// </summary>
     private void OnDestroy() {
         references.Remove(this);
     }
@@ -41,11 +50,24 @@ public class Localize_text : MonoBehaviour {
     #endregion
     #region ===== ===== API ===== =====
 
+    /// <summary>
+    /// changes the language of all referenced texts.
+    /// if the string in this Language is not found the id will be displayed.
+    /// </summary>
+    /// <param name="Language">the name of the Language as it is wirtten in the csv file</param>
     public void ChangeLanguage(string Language) {
-        if (_errorFlag)
-            return;
+        string temp = CSVReader.getValueAsStringFromStreamingAsset(_streamingAssetPath, _id, Language);
+        if (temp.Contains("ERROR")) {
+            foreach (var it in _textFeald) {
+                it.text = _id;
+            }
+        } else {
+            foreach (var it in _textFeald) {
+                it.text = temp;
+            }
+        }
 
-        _textFeald.text = CSVReader.getValueAsStringFromStreamingAsset(_streamingAssetPath, _id, Language);
+        
     }
 
     #endregion
--- UnityTools/Assets/Code/Tools/Localization/Test_Localization.cs
+++ UnityTools/Assets/Code/Tools/Localization/Test_Localization.cs
@@ -14,11 +14,18 @@ public class Test_Localization : MonoBehaviour {
             it.ChangeLanguage("German");
         }
 
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
 
         print("i'm changing the Language to English on " + Localize_text.references.Count + " instances.");
         foreach (var it in Localize_text.references) {
             it.ChangeLanguage("English");
+        }
+
+        yield return new WaitForSeconds(3);
+
+        print("i'm testing vor Latin whitch is not in the csv file");
+        foreach (var it in Localize_text.references) {
+            it.ChangeLanguage("Latin");
         }
     }
 }
