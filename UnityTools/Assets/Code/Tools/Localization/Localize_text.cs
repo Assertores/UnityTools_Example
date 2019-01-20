0f22167d30708cec2ec8d477d59de33bceb6578c
[JAM]Andreas <andreas.edmeier@web.de>
2019-01-20 12:41:32 +0100

    Localisation tool added
    only text yet

---
+++ UnityTools/Assets/Code/Tools/Localization/Localize_text.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// Localisation scripts must always have a public get list of Scriptreferences named 
+/// </summary>
+public class Localize_text : MonoBehaviour {
+
+    #region ===== ===== DATA ===== =====
+
+    public static List<Localize_text> references { get; private set; } = new List<Localize_text>();
+
+    [SerializeField] Text _textFeald;
+    [SerializeField] string _streamingAssetPath;
+    [SerializeField] string _id;
+
+    bool _errorFlag = false;
+
+    #endregion
+    #region ===== ===== REFRENCING ===== =====
+
+    private void Awake() {
+        if (!_textFeald) {
+            _textFeald = GetComponent<Text>();
+
+            if (!_textFeald) {
+                _errorFlag = true;
+                return;
+            }
+        }
+        references.Add(this);
+    }
+
+    private void OnDestroy() {
+        references.Remove(this);
+    }
+
+    #endregion
+    #region ===== ===== API ===== =====
+
+    public void ChangeLanguage(string Language) {
+        if (_errorFlag)
+            return;
+
+        _textFeald.text = CSVReader.getValueAsStringFromStreamingAsset(_streamingAssetPath, _id, Language);
+    }
+
+    #endregion
+}
---
+++ UnityTools/Assets/Code/Tools/Localization/Test_Localization.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Localization : MonoBehaviour {
+
+    void Start() {
+        StartCoroutine(Retest());
+    }
+
+    IEnumerator Retest() {
+        print("i'm changing the Language to German on " + Localize_text.references.Count + " instances.");
+        foreach(var it in Localize_text.references) {
+            it.ChangeLanguage("German");
+        }
+
+        yield return new WaitForSeconds(5);
+
+        print("i'm changing the Language to English on " + Localize_text.references.Count + " instances.");
+        foreach (var it in Localize_text.references) {
+            it.ChangeLanguage("English");
+        }
+    }
+}
