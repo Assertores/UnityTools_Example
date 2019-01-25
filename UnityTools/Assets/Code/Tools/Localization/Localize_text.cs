2485fbf79a76aee8b5f228cca56dac1c7a0b5247
Green Sky <green@g-s.xyz>
2019-01-25 13:20:43 +0100

    localization corrections

--- UnityTools/Assets/Code/Tools/Localization/Localize_text.cs
+++ UnityTools/Assets/Code/Tools/Localization/Localize_text.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 /// <summary>
 /// Localisation scripts must always have a public get list of Scriptreferences named references
-/// Localisation scripts must always have a public void funktion caled ChangeLanguage(string Language)
-/// this script requiers the CSVReader
+/// Localisation scripts must always have a public void function called ChangeLanguage(string Language)
+/// this script requires the CSVReader
 /// </summary>
 public class Localize_text : MonoBehaviour {
 
@@ -18,22 +18,22 @@ public class Localize_text : MonoBehaviour {
     /// </summary>
     public static List<Localize_text> references { get; private set; } = new List<Localize_text>();
 
-    [SerializeField] Text[] _textFeald; //reference to the Textfiel
-    [SerializeField] string _streamingAssetPath; //the path to the csv file whitch is used for the Localisation
+    [SerializeField] Text[] _textField; //reference to the Textfield
+    [SerializeField] string _streamingAssetPath; //the path to the csv file which is used for the Localisation
     [SerializeField] string _id; //the id of the string
 
     #endregion
     #region ===== ===== REFRENCING ===== =====
 
     /// <summary>
-    /// registers this script to the static List if it is valide
-    /// if _textFeald is not set, it tryes to find a text component in the gameobject
+    /// registers this script to the static List if it is valid
+    /// if _textField is not set, it tries to find a text component in the gameobject
     /// </summary>
     private void Awake() {
-        if (_textFeald.Length == 0) {
-            _textFeald = new Text[1] { GetComponent<Text>() };
+        if (_textField.Length == 0) {
+            _textField = new Text[1] { GetComponent<Text>() };
 
-            if (_textFeald.Length == 0) {
+            if (_textField.Length == 0) {
                 return;
             }
         }
@@ -54,20 +54,20 @@ public class Localize_text : MonoBehaviour {
     /// changes the language of all referenced texts.
     /// if the string in this Language is not found the id will be displayed.
     /// </summary>
-    /// <param name="Language">the name of the Language as it is wirtten in the csv file</param>
+    /// <param name="Language">the name of the Language as it is written in the csv file</param>
     public void ChangeLanguage(string Language) {
         string temp = CSVReader.getValueAsStringFromStreamingAsset(_streamingAssetPath, _id, Language);
         if (temp.Contains("ERROR")) {
-            foreach (var it in _textFeald) {
+            foreach (var it in _textField) {
                 it.text = _id;
             }
         } else {
-            foreach (var it in _textFeald) {
+            foreach (var it in _textField) {
                 it.text = temp;
             }
         }
 
-        
+
     }
 
     #endregion
