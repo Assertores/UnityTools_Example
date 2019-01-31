e0066bb12f7b84eeeef092def99683bf8034349e
[GA]Andreas <andreas.edmeier@web.de>
2019-02-01 00:04:47 +0100

    [PRO] AIBehaviro und Interface

---
+++ UnityTools/Assets/Code/Tools/WaightedAI/AIBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsserTOOLres {
+    public class AIBehavior : MonoBehaviour {
+
+        public static List<AIBehavior> _aIReverences = new List<AIBehavior>();
+
+        [SerializeField] IWaightedAIDesition[] _behaviors;
+
+        int _currentBehavior = -1;
+        
+        public void Reevaluate() {
+            _currentBehavior = -1;
+            float currentWaight = float.MinValue;
+            float tmp;
+            for (int i = 0; i < _behaviors.Length; i++) {
+                tmp = _behaviors[i].Evaluate();
+                if (tmp > currentWaight) {
+                    _currentBehavior = i;
+                    currentWaight = tmp;
+                }
+            }
+        }
+
+        void Update() {
+            if(_currentBehavior < 0 || _currentBehavior >= _behaviors.Length)
+                return;
+
+            _behaviors[_currentBehavior].Execute();
+        }
+    }
+}
---
+++ UnityTools/Assets/Code/Tools/WaightedAI/IWaightedAIDecition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsserTOOLres {
+    public interface IWaightedAIDesition {
+
+        /// <summary>
+        /// evaluates the importance of the Behavior
+        /// </summary>
+        /// <returns>the importance as a float value</returns>
+        float Evaluate();
+
+        /// <summary>
+        /// Executes one tick of the AI Behavior
+        /// </summary>
+        void Execute();
+    }
+}
